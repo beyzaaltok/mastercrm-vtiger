0253508652667a6cb56680b6b20406587300ad8d
sWIm-crm <sWIm-crm@protonmail.com>
2019-06-20 15:31:32 +0000

    Config backup

---
+++ test/templates_c/v7/4e988e86667a0c7e4780d8ebdf9be7f9320150d6.file.NoComments.tpl.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-06-20 15:30:58
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/NoComments.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:18383795925d0ba6b2c5aa09-65750918%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '4e988e86667a0c7e4780d8ebdf9be7f9320150d6' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/NoComments.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '18383795925d0ba6b2c5aa09-65750918',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'MODULE_NAME' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d0ba6b2c97da',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d0ba6b2c97da')) {function content_5d0ba6b2c97da($_smarty_tpl) {?>
+<div class="noCommentsMsgContainer noContent"><p class="textAlignCenter"> <?php echo vtranslate('LBL_NO_COMMENTS',$_smarty_tpl->tpl_vars['MODULE_NAME']->value);?>
+</p></div><?php }} ?>
