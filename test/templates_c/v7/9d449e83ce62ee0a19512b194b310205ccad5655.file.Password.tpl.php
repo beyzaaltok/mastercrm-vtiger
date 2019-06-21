4ad85bcca3fcf551918f41054fad5a0ae711bf02
sWIm-crm <sWIm-crm@protonmail.com>
2019-06-21 17:23:49 +0000

    Config backup

---
+++ test/templates_c/v7/9d449e83ce62ee0a19512b194b310205ccad5655.file.Password.tpl.php
@@ -0,0 +1,37 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-06-21 17:23:47
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/uitypes/Password.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:13594326495d0d12a3429eb8-27134723%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '9d449e83ce62ee0a19512b194b310205ccad5655' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/uitypes/Password.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '13594326495d0d12a3429eb8-27134723',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'FIELD_MODEL' => 0,
+    'MODULE' => 0,
+    'FIELD_NAME' => 0,
+    'SPECIAL_VALIDATOR' => 0,
+    'FIELD_INFO' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d0d12a344a7f',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d0d12a344a7f')) {function content_5d0d12a344a7f($_smarty_tpl) {?>
+<?php $_smarty_tpl->tpl_vars["FIELD_INFO"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldInfo(), null, 0);?><?php $_smarty_tpl->tpl_vars["SPECIAL_VALIDATOR"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getValidator(), null, 0);?><?php $_smarty_tpl->tpl_vars["FIELD_NAME"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('name'), null, 0);?><input id="<?php echo $_smarty_tpl->tpl_vars['MODULE']->value;?>
+_editView_fieldName_<?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
+" type="password" class="inputElement <?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->isNameField()){?>nameField<?php }?>"name="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldName();?>
+" value="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+"<?php if (!empty($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value)){?>data-validator='<?php echo Zend_Json::encode($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value);?>
+'<?php }?><?php if ($_smarty_tpl->tpl_vars['FIELD_INFO']->value["mandatory"]==true){?> data-rule-required="true" <?php }?><?php if (count($_smarty_tpl->tpl_vars['FIELD_INFO']->value['validator'])){?>data-specific-rules='<?php echo ZEND_JSON::encode($_smarty_tpl->tpl_vars['FIELD_INFO']->value["validator"]);?>
+'<?php }?>/><?php }} ?>
