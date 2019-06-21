ec86479c265f59820e053cbb16f0cf66c70ef9de
sWIm-crm <sWIm-crm@protonmail.com>
2019-06-21 18:39:19 +0000

    Config backup

---
+++ test/templates_c/v7/be1fc932b35a00f354a4ebc12fb99409f6524d59.file.ModuleSummaryView.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-06-21 18:39:17
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Accounts/ModuleSummaryView.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:19491939825d0d24551c7f96-83679143%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'be1fc932b35a00f354a4ebc12fb99409f6524d59' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Accounts/ModuleSummaryView.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '19491939825d0d24551c7f96-83679143',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'MODULE_NAME' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d0d24551ccf4',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d0d24551ccf4')) {function content_5d0d24551ccf4($_smarty_tpl) {?>
+
+<div class="recordDetails"><?php echo $_smarty_tpl->getSubTemplate (vtemplate_path('SummaryViewContents.tpl',$_smarty_tpl->tpl_vars['MODULE_NAME']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+</div><?php }} ?>
