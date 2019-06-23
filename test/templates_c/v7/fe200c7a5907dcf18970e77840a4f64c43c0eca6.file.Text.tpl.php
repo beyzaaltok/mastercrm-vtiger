eea6d9184595a40d42f5825cf54b35a7c58d2616
sWIm-crm <sWIm-crm@protonmail.com>
2019-06-23 09:46:14 +0000

    Config backup

---
+++ test/templates_c/v7/fe200c7a5907dcf18970e77840a4f64c43c0eca6.file.Text.tpl.php
@@ -0,0 +1,44 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-06-23 09:45:47
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Contacts/uitypes/Text.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:17216865395d0f4a4b452123-09179851%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'fe200c7a5907dcf18970e77840a4f64c43c0eca6' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Contacts/uitypes/Text.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '17216865395d0f4a4b452123-09179851',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'FIELD_MODEL' => 0,
+    'FIELD_NAME' => 0,
+    'FIELD_INFO' => 0,
+    'SPECIAL_VALIDATOR' => 0,
+    'MODULE_NAME' => 0,
+    'MODULE' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d0f4a4b4d621',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d0f4a4b4d621')) {function content_5d0f4a4b4d621($_smarty_tpl) {?>
+
+<?php $_smarty_tpl->tpl_vars["FIELD_INFO"] = new Smarty_variable(Zend_Json::encode($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldInfo()), null, 0);?><?php $_smarty_tpl->tpl_vars["SPECIAL_VALIDATOR"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getValidator(), null, 0);?><?php $_smarty_tpl->tpl_vars["FIELD_NAME"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldName(), null, 0);?><?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('uitype')=='19'||$_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('uitype')=='20'){?><textarea rows="3" class="inputElement textAreaElement col-lg-12 <?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->isNameField()){?>nameField<?php }?>" name="<?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
+" <?php if ($_smarty_tpl->tpl_vars['FIELD_NAME']->value=="notecontent"){?>id="<?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
+"<?php }?> data-validation-engine="validate[<?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->isMandatory()==true){?>required,<?php }?>funcCall[Vtiger_Base_Validator_Js.invokeValidation]]" data-fieldinfo='<?php echo $_smarty_tpl->tpl_vars['FIELD_INFO']->value;?>
+' <?php if (!empty($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value)){?>data-validator=<?php echo Zend_Json::encode($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value);?>
+<?php }?>><?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+</textarea><?php }else{ ?><textarea rows="5" class="inputElement <?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->isNameField()){?>nameField<?php }?>" name="<?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
+" data-validation-engine="validate[<?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->isMandatory()==true){?>required,<?php }?>funcCall[Vtiger_Base_Validator_Js.invokeValidation]]" data-fieldinfo='<?php echo $_smarty_tpl->tpl_vars['FIELD_INFO']->value;?>
+' <?php if (!empty($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value)){?>data-validator=<?php echo Zend_Json::encode($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value);?>
+<?php }?>><?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+</textarea><?php if ($_smarty_tpl->tpl_vars['MODULE_NAME']->value!='Webforms'&&$_REQUEST['view']!='Detail'){?><?php if ($_smarty_tpl->tpl_vars['FIELD_NAME']->value=="mailingstreet"){?><div><a class="cursorPointer" name="copyAddress" data-target="other"><?php echo vtranslate('LBL_COPY_OTHER_ADDRESS',$_smarty_tpl->tpl_vars['MODULE']->value);?>
+</a></div><?php }elseif($_smarty_tpl->tpl_vars['FIELD_NAME']->value=="otherstreet"){?><div><a class="cursorPointer" name="copyAddress" data-target="mailing"><?php echo vtranslate('LBL_COPY_MAILING_ADDRESS',$_smarty_tpl->tpl_vars['MODULE']->value);?>
+</a></div><?php }?><?php }?><?php }?><?php }} ?>
